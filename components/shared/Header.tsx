6a12ae668c14561f46c7643ef79dc552c6bfe32c
ali tarichi <tarichiali@gmail.com>
2024-05-06 18:02:12 +0300

    login & signup implanted using clerk

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,5 +1,7 @@
+import { SignedOut } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
+import { Button } from "../ui/button";
 
 const Header = () => {
   return (
@@ -13,7 +15,13 @@ const Header = () => {
             alt="evently logo"
           />
         </Link>
-        <div className="flex w-32 justify-end gap-3"></div>
+        <div className="flex w-32 justify-end gap-3">
+          <SignedOut>
+            <Button asChild className="rounded-full" size="lg">
+              <Link href="/sign-in">Login</Link>
+            </Button>
+          </SignedOut>
+        </div>
       </div>
     </header>
   );
