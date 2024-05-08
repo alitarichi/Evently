8791123b316bc256d60601fc23f824ff01dae5e1
ali tarichi <tarichiali@gmail.com>
2024-05-08 15:04:42 +0300

    clerkClient fixed?

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -2,7 +2,7 @@ import { Webhook } from "svix";
 import { headers } from "next/headers";
 import { WebhookEvent } from "@clerk/nextjs/server";
 import { createUser, deleteUser, updateUser } from "@/lib/actions/user.actions";
-import { clerkClient } from "@clerk/nextjs";
+import { clerkClient } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
