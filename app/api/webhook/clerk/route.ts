f9534fe4a0a6c815142a75b79ed6b248437a79e2
ali tarichi <tarichiali@gmail.com>
2024-05-08 15:44:00 +0300

    user error fixed?

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -63,8 +63,8 @@ export async function POST(req: Request) {
       clerkId: id,
       email: email_addresses[0].email_address,
       username: username!,
-      firstName: first_name,
-      lastName: last_name,
+      firstName: first_name || "",
+      lastName: last_name || "",
       photo: image_url,
     };
 
@@ -85,8 +85,8 @@ export async function POST(req: Request) {
     const { id, image_url, first_name, last_name, username } = evt.data;
 
     const user = {
-      firstName: first_name,
-      lastName: last_name,
+      firstName: first_name || "",
+      lastName: last_name || "",
       username: username!,
       photo: image_url,
     };
